73f0fdd178fad9c23235cd0f73368431ac47a5c9
Timothy Otholt <timotholt@gmail.com>
2025-03-24 18:31:25 -0700

    moved addslider code to ui/slider.js

--- v5/main.js
+++ v5/main.js
@@ -62,45 +62,7 @@ return false;
 };
 }
 
-function addSlider({
-               id,
-               name,
-               onUpdate,
-               options = {},
-               hidden = false,
-               showValue = true,
-               initialSpanValue = undefined,
-             }) {
-const div = document.createElement("div");
-div.style = `display: ${hidden ? "none": "flex"}; align-items: center; gap: 8px`;
-document.querySelector(`#${id}`).appendChild(div);
-div.append(`${name}`);
-const input = document.createElement("input");
-input.id = `${id}-${name.replace(" ", "-").toLowerCase()}-slider`;
-input.className = "slider";
-input.type = "range";
-Object.entries(options).forEach(([key, value]) => {
-input.setAttribute(key, value);
-});
-if (options.value) {
-input.value = options.value;
-}
-const span = document.createElement("span");
-input.setSpan = (value) => span.innerText = `${value}`;
-
-input.addEventListener("input", () => {
-input.setSpan(`${onUpdate(input.value)}`);
-});
-span.innerText = `${input.value}`;
-div.appendChild(input);
-div.appendChild(span);
-
-input.onUpdate = onUpdate;
-if (initialSpanValue != null) {
-input.setSpan(initialSpanValue);
-}
-return input;
-}
+import { addSlider } from './ui/slider.js';
 
 // @run
 
