09c30f638229edb0c74c7d5621351ef85bcceaa9
Timothy Otholt <timotholt@gmail.com>
2025-03-24 22:31:21 -0700

    updated comments in constants.js

--- v6/ui/constants.js
+++ v6/ui/constants.js
@@ -1,4 +1,6 @@
 // ASCII Font Constants
+// To change the font size, update these constants and provide a new font texture
+
 export const CHAR_PIXEL_SIZE = 8;  // Size of each character in pixels (8x8)
 export const GRID_SIZE = 16;       // 16x16 grid of characters
 export const TEXTURE_SIZE = 128;   // Total texture size (128x128)
