2da9f6135a0e765307c2fe3d0e8351a319222832
Timothy Otholt <timotholt@gmail.com>
2025-03-24 19:04:26 -0700

    moved webGlContext code into it's own file (context.js)

--- v5/main.js
+++ v5/main.js
@@ -5,6 +5,7 @@ import { GPUTimer} from './utils/gputimer.js';
 import { addSlider } from './ui/slider.js';
 import { onBuildReload } from './webgl/ReloadShaders.js';
 import { isMobile } from './utils/device.js';
+import { webGlContext, webGlInit } from './webgl/context.js';
 
 window.mdxishState = {
     startTime: new Date(),
@@ -303,65 +304,6 @@ this.updateTexture();
 
 
 
-function webGlContext() {
-const canvas = document.createElement('canvas');
-const w = new WebGL2MicroLayer(canvas);
-const pipeline = w.createPipeline();
-return { w, canvas, pipeline} ;
-}
-
-function webGlInit(
-context,
-width,
-height,
-materialProperties,
-renderTargetOverrides = {},
-extra = {}
-) {
-const { w, pipeline, canvas } = context;
-const dpr = extra.dpr || window.devicePixelRatio || 1;
-const scaling = dpr;
-const scale = extra.scale ? scaling : 1.0;
-const canvasScale = extra.canvasScale ?? 1.0;
-
-canvas.width = width * scaling;
-canvas.height = height * scaling;
-canvas.style.width = `${width * canvasScale}px`;
-canvas.style.height = `${height * canvasScale}px`;
-
-const renderTargetProps = {
-minFilter: w.gl.NEAREST,
-magFilter: w.gl.NEAREST,
-internalFormat: w.gl.RGBA16F,
-format: w.gl.RGBA,
-type: w.gl.HALF_FLOAT,
-...renderTargetOverrides
-};
-
-const renderTargetCount = extra?.renderTargetCount ?? 2;
-const renderTargets = [];
-
-for (let i = 0; i < renderTargetCount; i++) {
-renderTargets.push(
-w.createRenderTarget(width * scale, height * scale, renderTargetProps)
-);
-}
-
-const pass = pipeline.createPass(materialProperties, renderTargetProps.generateMipmaps);
-
-return {
-canvas,
-render: (uniforms = {}) => {
-pass.render(uniforms);
-},
-renderTargets,
-renderer: w,
-scaling,
-uniforms: pass.uniforms,
-gl: pipeline.w.gl,
-stage: pass,
-};
-}
 
 // @run
 class BaseSurface {
